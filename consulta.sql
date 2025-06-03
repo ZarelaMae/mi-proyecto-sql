3dd0eb551f099a3b0ee2032c68dbdb9b9bf3f00d
Zarela Mae <74084073.zmvj@gmail.com>
2025-06-02 19:03:59 -0500

    se agrego una tablita

--- consulta.sql
+++ consulta.sql
@@ -1,3 +1,5 @@
 create database tienda;
 create table cliente (id auto_increment primary key, nombre varchar(50), tipo varchar (50) not null default 'recurrente');
 insert into cliente(nombre) values ('Carla'),('Rosa'),('Pepita');
+
+create table productos (id auto_increment primary key, nombre varchar(50),stock int);
