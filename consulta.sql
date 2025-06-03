8ca70bd9cacefb541153b5769124dcbd22276960
ZarelaMae <74084073.zmvj@gmail.com>
2025-06-02 19:06:58 -0500

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -3,3 +3,4 @@ create table cliente (id auto_increment primary key, nombre varchar(50), tipo va
 insert into cliente(nombre) values ('Carla'),('Rosa'),('Pepita');
 
 create table productos (id auto_increment primary key, nombre varchar(50),stock int);
+insert into productos (nombre, stock) values ('Arroz',80),('Azucar','50');
